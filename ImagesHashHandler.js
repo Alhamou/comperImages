c9ca4e0df155d1c07c4f0918f1bde1b579daef7e
Emad Alhamou <emadalhamo@gmail.com>
2023-11-06 15:58:20 +0100

    add commeting to codes

--- ImagesHashHandler.js
+++ ImagesHashHandler.js
@@ -1,14 +1,19 @@
+// Import required modules: sharp for image processing, crypto for hashing, fs for file system operations
 const sharp = require('sharp');
 const crypto = require('crypto');
 const fs = require('fs').promises;
 
-const imagesHashHandler = (function(){
+// Define imagesHashHandler as an IIFE (Immediately Invoked Function Expression) to encapsulate and manage image hash operations
+const imagesHashHandler = (function() {
 
+  // Initialize an object to hold our public methods
   const obj = {}
 
+  // Define the file path for storing our hash images JSON
   const filePath = "./temp/hashImagesStored.json"
 
-  obj.verifyFilePresence = async function (targetPath) {
+  // Method to verify the presence of a file and create it if it does not exist
+  obj.verifyFilePresence = async function(targetPath) {
     try {
       await fs.access(targetPath);
     } catch (error) {
@@ -21,12 +26,14 @@ const imagesHashHandler = (function(){
     }
   }
 
+  // Method to read JSON data from a file, ensuring the file exists beforehand
   obj.readJsonData = async function() {
     await this.verifyFilePresence(filePath);
     const data = await fs.readFile(filePath, 'utf8');
     return JSON.parse(data || '[]');
   }
 
+  // Method to append new data to our JSON file, including reading and writing to the file
   obj.appendJsonData = async function(newData) {
     const existingData = await this.readJsonData(filePath);
     existingData.push(newData);
@@ -34,14 +41,15 @@ const imagesHashHandler = (function(){
     await fs.writeFile(filePath, jsonContent, 'utf8');
   }
 
+  // Method to calculate the hash of an image using SHA-256 algorithm
   obj.calculateImageHash = async function(imagePath) {
     const imageBuffer = await sharp(imagePath).raw().toBuffer();
     return crypto.createHash('sha256').update(imageBuffer).digest('hex');
   }
 
+  // Return our object with the public methods to be used outside this IIFE
   return obj;
 })()
 
-
-
-module.exports = {imagesHashHandler}
+// Export imagesHashHandler module for use in other parts of the application
+module.exports = { imagesHashHandler }
--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const {imagesHashHandler} = require('./ImagesHashHandler')
+// Import the imagesHashHandler from the ImagesHashHandler module to work with image hashes
+const { imagesHashHandler } = require('./ImagesHashHandler')
 
+// Self-invoking async function to use await at the top level
 ;(async () => {
 
+  // Define the path to the image you want to handle
   const imgPath = "./images/1.jpeg";
 
+  // Calculate the hash of the image using the calculateImageHash method from imagesHashHandler
   const imgHash = await imagesHashHandler.calculateImageHash(imgPath);
+  // Prepare image data with its path and calculated hash
   const imgData = { path: imgPath, hash: imgHash };
 
+  // Retrieve the existing image data from the JSON file
   const imagesData = await imagesHashHandler.readJsonData();
+  // Check if the image hash already exists in the data retrieved
   const foundImage = imagesData.find(item => item.hash === imgHash);
 
+  // If the image exists, log it to the console
   if (foundImage) {
     console.log("Image already exists in JSON:", foundImage);
   } else {
+    // If the image does not exist, append the new image data to the JSON file
     await imagesHashHandler.appendJsonData(imgData);
+    // Log a confirmation that new image data has been appended
     console.log("New image data appended to JSON.");
   }
-})();
+})(); // End of self-invoking function
