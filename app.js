20b0ff58759d24e478ca47bbe1af61112bc2a02b
Emad Alhamou <emadalhamo@gmail.com>
2023-11-06 15:20:44 +0100

    change names

--- app.js
+++ app.js
@@ -42,20 +42,20 @@ class ImagesHashHandler {
 }
 
 (async () => {
-  const fileMgr = new ImagesHashHandler("./temp/hashImagesStored.json");
+  const imagesHashHandler = new ImagesHashHandler("./temp/hashImagesStored.json");
   const imgPath = "./images/1.jpeg";
 
 
-  const imgHash = await fileMgr.calculateImageHash(imgPath);
+  const imgHash = await imagesHashHandler.calculateImageHash(imgPath);
   const imgData = { path: imgPath, hash: imgHash };
 
-  const imagesData = await fileMgr.readJsonData();
+  const imagesData = await imagesHashHandler.readJsonData();
   const foundImage = imagesData.find(item => item.hash === imgHash);
 
   if (foundImage) {
     console.log("Image already exists in JSON:", foundImage);
   } else {
-    await fileMgr.appendJsonData(imgData);
+    await imagesHashHandler.appendJsonData(imgData);
     console.log("New image data appended to JSON.");
   }
 })();
