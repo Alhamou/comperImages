8e1cce210e6bd2bbf8c2919cd0785f05d619ae88
Emad Alhamou <emadalhamo@gmail.com>
2023-11-06 15:17:22 +0100

    code adjusting

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const fs = require('fs').promises;
 
 class FileHandler {
 
+  constructor(filePath){
+    this.filePath = filePath
+  }
+
   async #verifyFilePresence(targetPath) {
     try {
       await fs.access(targetPath);
@@ -17,17 +21,17 @@ class FileHandler {
     }
   }
 
-  async readJsonData(filePath) {
-    await this.#verifyFilePresence(filePath);
-    const data = await fs.readFile(filePath, 'utf8');
+  async readJsonData() {
+    await this.#verifyFilePresence(this.filePath);
+    const data = await fs.readFile(this.filePath, 'utf8');
     return JSON.parse(data || '[]');
   }
 
-  async appendJsonData(filePath, newData) {
-    const existingData = await this.readJsonData(filePath);
+  async appendJsonData(newData) {
+    const existingData = await this.readJsonData(this.filePath);
     existingData.push(newData);
     const jsonContent = JSON.stringify(existingData, null, 2);
-    await fs.writeFile(filePath, jsonContent, 'utf8');
+    await fs.writeFile(this.filePath, jsonContent, 'utf8');
   }
 
   async calculateImageHash(imagePath) {
@@ -38,20 +42,20 @@ class FileHandler {
 }
 
 (async () => {
-  const fileMgr = new FileHandler();
+  const fileMgr = new FileHandler("./temp/hashImagesStored.json");
   const imgPath = "./images/1.jpeg";
-  const jsonFilePath = "./temp/hashImagesStored.json";
+
 
   const imgHash = await fileMgr.calculateImageHash(imgPath);
   const imgData = { path: imgPath, hash: imgHash };
 
-  const imagesData = await fileMgr.readJsonData(jsonFilePath);
+  const imagesData = await fileMgr.readJsonData();
   const foundImage = imagesData.find(item => item.hash === imgHash);
 
   if (foundImage) {
     console.log("Image already exists in JSON:", foundImage);
   } else {
-    await fileMgr.appendJsonData(jsonFilePath, imgData);
+    await fileMgr.appendJsonData(imgData);
     console.log("New image data appended to JSON.");
   }
 })();
